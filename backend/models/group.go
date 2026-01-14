298e8a20fa3f7b2da8a451f8a0170ae702c49bed
usama wahdin <wahdinzaid77@gmail.com>
2026-01-14 21:47:00 +0700

    add backend

--- backend/models/group.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Group struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Category string `json:"category"`
-	Items    int    `json:"items"`
-}
--- backend/models/item.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Item struct {
-	ID      int    `json:"id"`
-	GroupID int    `json:"group_id"`
-	Name    string `json:"name"`
-	Stock   int    `json:"stock"`
-}
